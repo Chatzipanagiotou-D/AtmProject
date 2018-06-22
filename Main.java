7e6a599ef3180f88c5a21e71c579edb9291670e5
Chatzipanagiotou-D <40489475+Chatzipanagiotou-D@users.noreply.github.com>
2018-06-22 18:21:43 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Operations f = new Operations("Open Bank System");
+		f.addCustomer(16850, "DIMITRIOS", "CHATZIPANAGIOTOU", "29/01/1989", 0, 1000, 1015);
+		f.addCustomer(16840, "JOHN", "BATTURO", "20/05/1988", 0, 100, 1285);
+		f.addCustomer(19487, "NICK", "ANTWNIOU", "01/05/1945", 0, 6100, 5550);
+		f.addCustomer(47888, "DIMITRIOS", "DEM", "29/01/1980", 0, 5400, 1512);
+		f.addCustomer(168540, "MELLISA", "BATTURO", "02/05/1990", 0, 5000, 1214);
+		f.addCustomer(168501, "JOAN", "ANTONIOU", "05/12/1990", 0, 7788, 9568);
+		
+		System.out.println();
+    
